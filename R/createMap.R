297039d10f91e2b6dba4a3df71b1b3461d3e9042
viana-guilherme <viana.guilherme@pm.me>
2022-05-19 12:42:43 -0300

    add function to create empty plate layout as .xlsx

---
+++ R/createMap.R
@@ -0,0 +1,30 @@
+#' Generate a 96-well map template
+#' @param map_name A string specifying the name for the map template.
+#' @param interactive Use interactive mode to design template
+#' @export
+#'
+createMap <- function(map_name, interactive = FALSE) {
+
+  column_names <- c(NULL, c(paste0("0", 1:9), 10, 11, 12))
+  row_names <- c(NULL, LETTERS[1:8])
+
+  out_table <- matrix(nrow = 8, ncol = 12) |>
+                magrittr::set_colnames(paste0('"',column_names,'"')) |>
+                magrittr::set_rownames(row_names) |>
+                tibble::as_tibble(rownames = " ")
+
+  out_path = file.path(here::here(),paste0(map_name,".xlsx"))
+
+  writexl::write_xlsx(x = out_table,
+                      format_headers = FALSE,
+                      path = out_path)
+
+  message(paste0(" Map template created in ", here::here(), '!'))
+
+
+  if(interactive) { message("ATIVADO") }
+
+ }
+
+
+
