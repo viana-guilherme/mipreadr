5abcbc2fa2ce26062f0e080f2154134e7d96783a
viana-guilherme <viana.guilherme@pm.me>
2022-09-22 14:37:33 -0300

    gadget function now compatible with createLayout

--- R/templateGadget.R
+++ R/templateGadget.R
@@ -1,7 +1,7 @@
-templateGadget <- function(template_path, template_name = "") {
+templateGadget <- function(layout_path, layout_name = "") {
 
   ui <- miniUI::miniPage(
-    miniUI::gadgetTitleBar(glue::glue("x3 Template Editor - {template_name}")),
+    miniUI::gadgetTitleBar(glue::glue("x3 Template Editor - {layout_name}")),
     shiny::fillPage(
     miniUI::miniContentPanel(scrollable = FALSE, padding = 10,
     rhandsontable::rHandsontableOutput("rTable")))
@@ -9,7 +9,7 @@ templateGadget <- function(template_path, template_name = "") {
 
   server <- function(input, output, session){
 
-    plate_layout <- suppressMessages(readxl::read_excel(template_path, col_types = "text")) |>
+    plate_layout <- suppressMessages(readxl::read_excel(layout_path, col_types = "text")) |>
       tibble::column_to_rownames("...1")
 
     output$rTable <- rhandsontable::renderRHandsontable({
@@ -24,7 +24,7 @@ templateGadget <- function(template_path, template_name = "") {
 
       writexl::write_xlsx(x = out_data,
                           format_headers = FALSE,
-                          path = template_path)
+                          path = layout_path)
       shiny::stopApp()
     })
 
