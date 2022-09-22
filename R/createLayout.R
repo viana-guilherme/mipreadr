9749eafa34d97a0a1bdcf87d38f77a84d7ab4443
viana-guilherme <viana.guilherme@pm.me>
2022-09-22 14:35:45 -0300

    updated createTemplate to createLayout and fixed inconsistencies

--- R/createLayout.R
+++ R/createLayout.R
@@ -1,10 +1,10 @@
 #' Generate a 96-well map template
-#' @param template_name A string specifying the name for the map template.
+#' @param layout_name A string specifying the name for the map template.
 #' @param interactive Use interactive mode to design template
 #' @export
 #'
 
-createTemplate <- function(template_name, interactive = FALSE) {
+createLayout <- function(layout_name, interactive = FALSE) {
 
   column_names <- c(NULL, c(paste0("0", 1:9), 10, 11, 12))
   row_names <- c(NULL, LETTERS[1:8])
@@ -14,15 +14,15 @@ createTemplate <- function(template_name, interactive = FALSE) {
                 magrittr::set_rownames(row_names) |>
                 tibble::as_tibble(rownames = " ")
 
-  out_path <- file.path(here::here(),paste0(template_name,".xlsx"))
+  out_path <- file.path(here::here(),paste0(layout_name,".xlsx"))
 
   writexl::write_xlsx(x = out_table,
                       format_headers = FALSE,
                       path = out_path)
 
-  message(glue::glue("Template for plate {template_name} created in {here::here()}!"))
+  message(glue::glue("Template for plate {layout_name} created in {here::here()}!"))
 
-  if(interactive) { templateGadget(out_path, template_name = template_name) }
+  if(interactive) { templateGadget(out_path, layout_name = layout_name) }
 
  }
 
